13f37db66fef3edd186a20fecb9637ef32ea53af
HAGELST0N <patrick.hagelston@gmail.com>
2025-11-08 17:11:10 -0500

    Added function to create Perlin noise.

--- PerlinWorms/Patrick.cpp
+++ PerlinWorms/Patrick.cpp
@@ -1,3 +1,38 @@
+
+
 //
 // Created by patri on 10/30/2025.
 //
+
+#include "FastNoiseLite.h"
+#include <vector>
+using namespace std;
+
+// generates vector of floats from -1 to 1 representing noise
+// x_size, y_size: the size of the vector to generate
+// seed: the seed used when generating noise
+vector<float> createNoise(const int x_size, const int y_size, const int seed){
+     // following modified from https://github.com/Auburn/FastNoiseLite/tree/master/Cpp#readme
+     // the modifications allow it to be called as a function with parameters
+
+     // initializing noise as perlin noise
+     // Create and configure FastNoise object
+     FastNoiseLite noise;
+     noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+     // modified to use seed parameter to set seed when called
+     noise.SetSeed(seed);
+
+     // Gather noise data
+     // modified to use x_size and y_size
+     vector<float> noiseData(x_size * y_size);
+     int index = 0;
+
+     for (int y = 0; y < y_size; y++)
+     {
+          for (int x = 0; x < x_size; x++)
+          {
+               noiseData[index++] = noise.GetNoise((float)x, (float)y);
+          }
+     }
+     // Do something with this data...
+}
